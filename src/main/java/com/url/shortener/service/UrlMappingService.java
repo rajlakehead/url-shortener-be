6a06044b9ea8802799994277bcaec54ed6610677
Raj Patel <patelraj24365@gmail.com>
2025-03-21 12:39:39 -0400

    setup url redirects

--- src/main/java/com/url/shortener/service/UrlMappingService.java
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -89,4 +89,20 @@ public class UrlMappingService {
                 .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
 
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            // Record Click Event
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+
+        return urlMapping;
+    }
 }
